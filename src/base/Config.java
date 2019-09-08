3149fecfb1c9bf31c460116334794584924d8b20
Golam Sadek <sadekny@gmail.com>
2019-09-08 15:20:42 -0400

    3rd

--- src/base/Config.java
+++ src/base/Config.java
@@ -1,5 +1,25 @@
 package base;
 
-public class Config {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+import utils.SWDFunctions;
+
+public class Config extends SWDFunctions {
+
+	@BeforeTest
+	public void beforeTestStart(){
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver");	
+		WebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.com/");
+
+	}
 }
--- src/test_Amazon/TC1_SearchForProducts.java
+++ src/test_Amazon/TC1_SearchForProducts.java
@@ -1,5 +1,24 @@
 package test_Amazon;
 
-public class TC1_SearchForProducts {
+import org.testng.annotations.Test;
+
+import base.Config;
+import locators.Amazon_Search_Loc;
+import values.Amazon_Search_Values;
+
+public class TC1_SearchForProducts extends Config {
+
+
+	//Amazon_Search_Loc date1 = new Amazon_Search_Loc();
+	//Amazon_Search_Values date2 = new Amazon_Search_Values();
+
+	@Test
+	public void SearchForMusic() { 
+		System.out.println("Search for Music");
+		
+		
+
+	}
+	
 
 }
