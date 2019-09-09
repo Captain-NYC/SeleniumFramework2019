8278be7976336802216fce87f1b09aee0a711b82
Golam Sadek <sadekny@gmail.com>
2019-09-09 00:06:36 -0400

    4th push

--- src/base/Config.java
+++ src/base/Config.java
@@ -1,25 +1,26 @@
 package base;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
-import utils.SWDFunctions;
+public class Config {
+	WebDriver driver;
 
-public class Config extends SWDFunctions {
+	@BeforeMethod
+	public void beforeEachMethod(){
+		System.out.println("Before each method");
+		System.setProperty("webdriver.chrome.driver","‎⁨/Users⁩/Sadek⁩/⁨Documents⁩/GitHub⁩/⁨SeleniumFramework2019/drivers/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.com");
+	}
 
-	@BeforeTest
-	public void beforeTestStart(){
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver");	
-		WebDriver driver = new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-		driver.get("https://www.amazon.com/");
+	@AfterMethod
+	public void afterEachMethod(){
+		System.out.println("After each method");
+		System.out.println (" ==========  ");
+		driver.quit();
 
 	}
 }
--- src/test_Amazon/TC1_SearchForProducts.java
+++ src/test_Amazon/TC1_SearchForProducts.java
@@ -3,22 +3,47 @@ package test_Amazon;
 import org.testng.annotations.Test;
 
 import base.Config;
-import locators.Amazon_Search_Loc;
-import values.Amazon_Search_Values;
 
 public class TC1_SearchForProducts extends Config {
 
-
-	//Amazon_Search_Loc date1 = new Amazon_Search_Loc();
-	//Amazon_Search_Values date2 = new Amazon_Search_Values();
+	// In this class, we are running each test using @Test - TestNG annotation
 
 	@Test
-	public void SearchForMusic() { 
-		System.out.println("Search for Music");
-		
-		
+	public void amazonSearchForMusic(){
+		System.out.println(" this is amazon MUSIC search test ");
+		// enter "music" in edit/search box
+		// click search button
+		// assert if you see music in results or not
+		//String exp = "music";
+		//String act = "//*[@id="search"]/span/h1/div/div[1]/div/div/span[3]";
+		// assertion - verify
+		//driver.quit();
 
 	}
-	
+	@Test
+	public void amazonSearchForMovie(){
+		System.out.println(" this is amazon MOVIE search test ");
+		// enter "movies" in edit/search box
+		// click search button
+		// assert if you see music in results or not
+		//String exp = "music";
+		//String act = "//*[@id="search"]/span/h1/div/div[1]/div/div/span[3]";
+		// assertion - verify
+
+	}
+	@Test
+	public void amazonSearchForCD(){
+		System.out.println(" this is amazon CD search test ");
+		// enter "cd" in edit/search box
+		// click search button
+		// assert if you see music in results or not
+		//String exp = "music";
+		//String act = "//*[@id="search"]/span/h1/div/div[1]/div/div/span[3]";
+		// assertion - verify
+
+
+
+	}
+
 
 }
